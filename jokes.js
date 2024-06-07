6fc65aa21ba002cd1f0376b72fefd0a00557b6c1
Nishit Grover <grovernt@mail.uc.edu>
2024-06-07 12:24:31 -0400

    chore: Add Bootstrap 5.3.3 dependency and update styles

--- jokes.js
+++ jokes.js
@@ -1,20 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const jokeButton = document.getElementById("get-joke");
-  const jokeText = document.getElementById("joke-text");
+const jokeButton = document.getElementById("get-joke");
+const jokeText = document.getElementById("joke-text");
 
-  jokeButton.addEventListener("click", function () {
-    fetchJoke();
-  });
-
-  function fetchJoke() {
-    fetch("https://api.api-ninjas.com/v1/dadjokes")
-      .then((response) => response.json())
-      .then((data) => {
-        jokeText.textContent = data.joke;
-      })
-      .catch((error) => {
-        jokeText.textContent = "Failed to fetch joke. Please try again later.";
-        console.error("Error fetching joke:", error);
-      });
-  }
+jokeButton.addEventListener("click", function () {
+  fetchJoke();
 });
+
+const API_KEY = "U+UQiJVSK9DUUsrzk8jm7g==KO2ddJH4SJXDazoE";
+const API_URL = "https://api.api-ninjas.com/v1/dadjokes";
+
+function fetchJoke() {
+  fetch(API_URL, {
+    headers: {
+      "X-Api-Key": API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      jokeText.textContent = data[0].joke;
+    })
+    .catch((error) => {
+      jokeText.textContent = "Failed to fetch joke. Please try again later.";
+      console.error("Error fetching joke:", error);
+    });
+}
