2c917adcd0da075e367d604e897273d891c1a8c3
Nishit Grover <grovernt@mail.uc.edu>
2024-06-07 19:37:08 -0400

    chore: Refactor joke fetching functions and add event listeners

--- jokes.js
+++ jokes.js
@@ -1,70 +1,72 @@
 import config from "./config.js";
 
-const dadButton = document.getElementById("dad-joke");
-const jokeButton = document.getElementById("normal-joke");
-const factButton = document.getElementById("get-fact");
+document.addEventListener("DOMContentLoaded", function () {
+  const dadButton = document.getElementById("dad-joke");
+  const jokeButton = document.getElementById("normal-joke");
+  const factButton = document.getElementById("get-fact");
 
-const jokeText = document.getElementById("joke-text");
+  const jokeText = document.getElementById("joke-text");
 
-dadButton.addEventListener("click", function () {
-  fetchDadJoke();
-});
+  dadButton.addEventListener("click", function () {
+    fetchDadJoke();
+  });
 
-function fetchDadJoke() {
-  fetch(config.API_URL_DAD, {
-    headers: {
-      "X-Api-Key": config.API_KEY,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      jokeText.textContent = data[0].joke;
+  function fetchDadJoke() {
+    fetch(config.API_URL_DAD, {
+      headers: {
+        "X-Api-Key": config.API_KEY,
+      },
     })
-    .catch((error) => {
-      jokeText.textContent = "Failed to fetch joke. Please try again later.";
-      console.error("Error fetching joke:", error);
-    });
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        jokeText.textContent = data[0].joke;
+      })
+      .catch((error) => {
+        jokeText.textContent = "Failed to fetch joke. Please try again later.";
+        console.error("Error fetching joke:", error);
+      });
+  }
 
-jokeButton.addEventListener("click", function () {
-  fetchJoke();
-});
+  jokeButton.addEventListener("click", function () {
+    fetchJoke();
+  });
 
-function fetchJoke() {
-  fetch(config.API_URL, {
-    headers: {
-      "X-Api-Key": config.API_KEY,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      jokeText.textContent = data[0].joke;
+  function fetchJoke() {
+    fetch(config.API_URL, {
+      headers: {
+        "X-Api-Key": config.API_KEY,
+      },
     })
-    .catch((error) => {
-      jokeText.textContent = "Failed to fetch joke. Please try again later.";
-      console.error("Error fetching joke:", error);
-    });
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        jokeText.textContent = data[0].joke;
+      })
+      .catch((error) => {
+        jokeText.textContent = "Failed to fetch joke. Please try again later.";
+        console.error("Error fetching joke:", error);
+      });
+  }
 
-factButton.addEventListener("click", function () {
-  fetchFact();
-});
+  factButton.addEventListener("click", function () {
+    fetchFact();
+  });
 
-function fetchFact() {
-  fetch(config.API_URL_FACT, {
-    headers: {
-      "X-Api-Key": config.API_KEY,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      jokeText.textContent = data[0].fact;
+  function fetchFact() {
+    fetch(config.API_URL_FACT, {
+      headers: {
+        "X-Api-Key": config.API_KEY,
+      },
     })
-    .catch((error) => {
-      jokeText.textContent = "Failed to fetch joke. Please try again later.";
-      console.error("Error fetching joke:", error);
-    });
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        jokeText.textContent = data[0].fact;
+      })
+      .catch((error) => {
+        jokeText.textContent = "Failed to fetch joke. Please try again later.";
+        console.error("Error fetching joke:", error);
+      });
+  }
+});
