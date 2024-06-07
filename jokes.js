11af32ebc75792a3e32cf01a8251a4fb2c256855
Nishit Grover <grovernt@mail.uc.edu>
2024-06-07 12:46:03 -0400

    chore: Update script tag in index.html to use module type

--- jokes.js
+++ jokes.js
@@ -1,23 +1,67 @@
-const jokeButton = document.getElementById("get-joke");
+import config from "./config";
+
+const dadButton = document.getElementById("dad-joke");
+const jokeButton = document.getElementById("normal-joke");
+const factButton = document.getElementById("get-fact");
+
 const jokeText = document.getElementById("joke-text");
+
+dadButton.addEventListener("click", function () {
+  fetchDadJoke();
+});
+
+function fetchDadJoke() {
+  fetch(config.API_URL_DAD, {
+    headers: {
+      "X-Api-Key": config.API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      jokeText.textContent = data[0].joke;
+    })
+    .catch((error) => {
+      jokeText.textContent = "Failed to fetch joke. Please try again later.";
+      console.error("Error fetching joke:", error);
+    });
+}
 
 jokeButton.addEventListener("click", function () {
   fetchJoke();
 });
 
-const API_KEY = "U+UQiJVSK9DUUsrzk8jm7g==KO2ddJH4SJXDazoE";
-const API_URL = "https://api.api-ninjas.com/v1/dadjokes";
-
 function fetchJoke() {
-  fetch(API_URL, {
+  fetch(config.API_URL, {
     headers: {
-      "X-Api-Key": API_KEY,
+      "X-Api-Key": config.API_KEY,
     },
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
       jokeText.textContent = data[0].joke;
+    })
+    .catch((error) => {
+      jokeText.textContent = "Failed to fetch joke. Please try again later.";
+      console.error("Error fetching joke:", error);
+    });
+}
+
+factButton.addEventListener("click", function () {
+  fetchFact();
+});
+
+function fetchFact() {
+  fetch(config.API_URL_FACT, {
+    headers: {
+      "X-Api-Key": config.API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      jokeText.textContent = data[0].fact;
     })
     .catch((error) => {
       jokeText.textContent = "Failed to fetch joke. Please try again later.";
